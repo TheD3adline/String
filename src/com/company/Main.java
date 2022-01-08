8fd43421120d095dfbad34a98a2526d539ac0953
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-08 12:48:11 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String test = "Michael Fessler";
+
+        System.out.println("Namen: " + test + " ist der Hammer!");
+
+    }
+}
